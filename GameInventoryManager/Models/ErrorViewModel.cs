929d9e3847ae146ecf608e440feb8a45e6ca1bab
Aubry <Aubry@PROBOOK>
2023-11-06 23:05:54 +0100

    Ajoutez des fichiers projet.

---
+++ GameInventoryManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GameInventoryManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
